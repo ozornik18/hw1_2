1a45314305a2b51b08bd2e2c4367f916c5d9f3ac
Константин <kuz18rus@ya.ru>
2022-12-04 15:48:50 +0400

    Выполение заданий 1 - 5

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,71 @@ public class Main {
     public static void main(String[] args) {
         // Задача 1
         System.out.println("Задача 1");
+        byte cat = 40;
+        System.out.println(cat);
+        short b = 200;
+        System.out.println(b);
+        int c = 500;
+        System.out.println(c);
+        long d = 564789;
+        System.out.println(d);
+        float e = 2.2f;
+        System.out.println(e);
+        double i = 7.8;
+        System.out.println(i);
+        boolean catIsAdult = cat < b;
+        System.out.println(cat);
+        char bar = 35;
+        System.out.println(bar);
+
+        // Задача 2
+        System.out.println("Задача 2");
+        double x = 27.12;
+        long y = 987678965;
+        byte v = 2;
+        int f = 786;
+        boolean isAdult = f >= x;
+        int apple = 569;
+        int temperature = -159;
+        int weight = 27897;
+        double apple2 = 67.0;
+
+        // Задача 3
+        System.out.println("Задача 3");
+        int totalPaper = 480;
+        int class1 = 23;
+        int class2 = 27;
+        int class3 = 30;
+        int total = class1 + class2 + class3;
+        System.out.println("На каждого ученика рассчитано " + totalPaper / total + " листов бумаги");
+
+        // Задача 4
+        System.out.println("Задача 4");
+        int bottle = 16;
+        int time2 = 2;
+        int time20 = 20;
+        int totalBottle1 = bottle / time2;
+        int totalBottle2 = totalBottle1 * time20;
+        System.out.println("За " + time20 + " минут машина произвела бутылок " + totalBottle2 + " штук");
+
+        // Задача 5
+        System.out.println("Задача 5");
+        int paint = 120;
+        int white = 2;
+        int brown = 4;
+        int totalClass = paint / (white + brown);
+        System.out.println("В школе, где " + totalClass + " классов, нужно " + totalClass * white + " банок белой краски и " + totalClass * brown + " банок коричневой краски");
+
+
+        // Задача 6
+        System.out.println("Задача 6");
+
+        // Задача 7
+        System.out.println("Задача 7");
+
+        // Задача 8
+        System.out.println("Задача 8");
+
     }
 
 }
